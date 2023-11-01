b40b22fa5479dbb9a624274ef9ffb868cf3e1e86
MarilenaChochlidaki <marilenachochlidaki@gmail.com>
2023-11-01 20:59:46 +0200

    testing

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          me lene teri
+          me lene terilef
         </p>
         <a
           className="App-link"
